cc4f17d6b7e35e74d16efa568c0c37a6c7596926
Alexey <sulfur1917@gmail.com>
2024-06-25 13:47:49 +0300

    add routes to login and registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { ApplicationConfig } from "@angular/core";
 
-export const routes: Routes = [];
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {NotFoundComponent} from "./not_found/not-found.component";
+
+export const appRoutes: Routes =[
+    { path: "", component: HomeComponent},
+    { path: "login", component: LoginComponent},
+    { path: "register", component: RegisterComponent},
+    { path: "**", component: NotFoundComponent }
+];
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(appRoutes)]
+};
